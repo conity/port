cc41727024ed3b1410973ee6295fa9ccfe2f7ac1
conity <54306647+conity@users.noreply.github.com>
2023-09-05 19:23:43 +0700

    Update port.sh

--- port.sh
+++ port.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-# 交互式输入本地端口
-read -p "请输入本地监听端口号: " LOCAL_PORT
+read -p "请输入要转发的本地端口号： " local_port
+read -p "请输入目标服务器的 IP 地址： " target_ip
+read -p "请输入目标服务器的端口号： " target_port
 
-# 交互式输入目标 IP
-read -p "请输入目标 IP 地址: " DEST_IP
+# 更新防火墙规则
+if [ -x "$(command -v firewall-cmd)" ]; then
+  # CentOS 防火墙规则更新
+  firewall-cmd --add-forward-port=port="$local_port":proto=tcp:toaddr="$target_ip":toport="$target_port"
+elif [ -x "$(command -v ufw)" ]; then
+  # Ubuntu 防火墙规则更新
+  ufw allow "$local_port"/tcp
+  iptables -t nat -A PREROUTING -p tcp --dport "$local_port" -j DNAT --to-destination "$target_ip":"$target_port"
+else
+  echo "无法确定系统类型或防火墙工具不存在"
+  exit 1
+fi
 
-# 交互式输入目标端口
-read -p "请输入目标端口号: " DEST_PORT
+# 启用IP转发
+echo "1" > /proc/sys/net/ipv4/ip_forward
 
-# 清除所有已经存在的转发规则
-iptables -F
-
-# 启用内核 IP 转发
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# 设置转发规则，将外部请求的 LOCAL_PORT 端口转发到内部的 DEST_IP 的 DEST_PORT 端口
-iptables -t nat -A PREROUTING -p tcp --dport $LOCAL_PORT -j DNAT --to-destination $DEST_IP:$DEST_PORT
+# 配置端口转发
+iptables -t nat -A PREROUTING -p tcp --dport "$local_port" -j DNAT --to-destination "$target_ip":"$target_port"
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
-# 保存规则
-iptables-save > /etc/iptables/rules.v4
+# 保存防火墙规则
+if [ -x "$(command -v firewall-cmd)" ]; then
+  # CentOS 保存防火墙规则
+  firewall-cmd --runtime-to-permanent
+elif [ -x "$(command -v ufw)" ]; then
+  # Ubuntu 保存防火墙规则
+  ufw reload
+else
+  echo "无法确定系统类型或防火墙工具不存在"
+  exit 1
+fi
 
-# 打开防火墙端口
-ufw allow $LOCAL_PORT
-
-# 启用防火墙
-ufw enable
-
-echo "端口转发已设置完成！"
+echo "端口转发已经成功设置！"
